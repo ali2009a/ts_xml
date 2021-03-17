d72e81f1aadad05a8fa590fae91529b7e54e61fc
ali2009a <aliarab2009@gmail.com>
2021-03-17 02:04:46 -0700

    Working version of interpret with a gradient based method

--- interpret.py
+++ interpret.py
@@ -2,21 +2,16 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 import sys, os
-sys.path.append("../../Scripts")
-from model import Transformer,TCN,LSTMWithInputCellAttention,LSTM
 import numpy as np
 import argparse
 import random
-from utils import data_generator
-import Helper
-from Plotting import plotExampleBox
 from torch.autograd import Variable
 from  sklearn import preprocessing
 import data
 import warnings
 from  sklearn.preprocessing import minmax_scale
 import matplotlib.pyplot as plt
-
+from tqdm import tqdm
 warnings.filterwarnings("ignore")
 
 from captum.attr import (
@@ -48,34 +43,40 @@ def givenAttGetRescaledSaliency(args,attributions,isTensor=True):
 
 
 def main():
-    
-    model_filename ="../Models/m_k_1_model_TCN_NumFeatures_3000.pt"
+    model_filename ="../Models/m_k_7_model_TCN_NumFeatures_3000.pt"
     pretrained_model = torch.load(open(model_filename, "rb"),map_location=device) 
     pretrained_model.to(device)    
-
-
     dataset = data.HDF5Dataset("ts_data.h5")
     dataloader = data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0) 
-    data, target = next(iter(dataloader))
-    labels =  target.to(device)
-    input = data.to(device)
-    input = Variable(input,  volatile=False, requires_grad=True)
-
-    Data=data.reshape(3000, 100).data.cpu().numpy()
-    
-    target_=target.data.cpu().numpy()[0]
-    Grad = Saliency(pretrained_model)
-    attributions = Grad.attribute(input)
-    args=""
-    saliency_=Helper.givenAttGetRescaledSaliency(args,attributions)    
-    np.save("res2.npy", saliency_)
+    arrays = []
+    for x_data , target in tqdm(dataloader):
+        #data, target = next(iter(dataloader))
+        labels =  target.to(device)
+        input = x_data.to(device)
+        input = Variable(input,  volatile=False, requires_grad=True)
+        Data= x_data.reshape(3000, 100).data.cpu().numpy()
+        target_=target.data.cpu().numpy()[0]
+        Grad = Saliency(pretrained_model)
+        attributions = Grad.attribute(input)
+        args=""
+        saliency_= givenAttGetRescaledSaliency(args,attributions)    
+        arrays.append(saliency_)
+    np.save("res2.npy", arrays)
+    #aggr = np.mean(arrays, axis=0)    
  
 
+def main_plot():
+    arrays = np.load("res2.npy")
+    aggr = np.mean(arrays, axis=0)
+    aggr= aggr.reshape(60,50,100)
+    for i in range(6):
+        plot(aggr, i*10,i*10+10, "cluster_{}.png".format(i))
 
-def plot(images,n, fileName):
-    f, ax = plt.subplots(2,1,sharex=True,figsize=(8,12))
+def plot(images,s,e, fileName):
+    n=e-s
+    f, ax = plt.subplots(n,1,sharex=True,figsize=(8,12))
     for i in range(n):
-        ax[0].imshow(images[i])
+        ax[i].imshow(images[s+i])
     plt.savefig(fileName)
         
 
